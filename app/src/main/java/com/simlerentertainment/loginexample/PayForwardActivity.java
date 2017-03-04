1c738106bbe56c271b046b17e0997eeedca68af9
Eitan Simler <eitan.simler@gmail.com>
2017-03-03 23:22:20 -0800

    Changed textType

--- app/src/main/java/com/simlerentertainment/loginexample/PayForwardActivity.java
+++ app/src/main/java/com/simlerentertainment/loginexample/PayForwardActivity.java
@@ -1,8 +1,11 @@
 package com.simlerentertainment.loginexample;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 
 public class PayForwardActivity extends AppCompatActivity {
@@ -22,4 +25,24 @@ public class PayForwardActivity extends AppCompatActivity {
         mID_TextView.setText(name);
     }
 
+    public void onButtonClick(View view) {
+        final AlertDialog alertDialog = new AlertDialog.Builder(PayForwardActivity.this).create();
+        alertDialog.setTitle("Do you want to sign out?");
+        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "NO",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        alertDialog.dismiss();
+                    }
+                });
+        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Yes",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Intent intent = new Intent(getApplicationContext(), PhoneNumberActivity.class);
+                        startActivity(intent);
+                    }
+                });
+        alertDialog.show();
+    }
 }
